875464931c5f03ae96a89ccff54cc86747dbcca7
garnere1 <emilygarner24@gmail.com>
2024-08-06 19:44:39 -0400

    bootstrap html/css for now

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 from src.orchestrator import create_chatbot
+from src.classes import chain
 
 load_dotenv()
 
@@ -17,10 +18,16 @@ def home_page():
 def submit():
     if request.method == 'POST':
         created_chain = create_chatbot(name = request.form['name'], mood = request.form['mood'])
-        answer = created_chain.invoke_chain("hello")
-        return render_template("chatbot.html", answer = answer.content)
+        return render_template("chatbot.html")
     elif request.method == 'GET':
         return render_template("home.html", options = moods)
+
+@app.route("/query", methods=['POST'])
+def query():
+    created_chain = chain.get_chain()
+    question = request.form['question']
+    answer = created_chain.invoke_chain(question)
+    return answer
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
